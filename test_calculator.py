22d9a8bb23e515eb080fe0668e5163d2cb5ab1c9
Jatin Sukhija <sukhijajatin@gmail.com>
2025-03-01 14:17:17 +0530

    Python Calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,14 @@
 import pytest
 from calculator import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_subtract():
     assert subtract(10, 5) == 5
+
 
 def test_multiply():
     assert multiply(3, 4) == 12
